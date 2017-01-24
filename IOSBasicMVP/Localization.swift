f6c40ad9959d311c02a05f72dd362a0ad46fc6a0
Pablo Jiménez <pabjimcas@gmail.com>
2017-01-24 17:33:16 +0100

    first commit

---
+++ IOSBasicMVP/Localization.swift
@@ -0,0 +1,9 @@
+//
+//  Localization.swift
+//  IOSBasicMVP
+//
+//  Created by Pablo Jiménez on 23/1/17.
+//  Copyright © 2017 Pablo Jiménez. All rights reserved.
+//
+
+import Foundation
---
+++ IOSBasicMVP/PresentersProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  PresentersProtocols.swift
+//  IOSBasicMVP
+//
+//  Created by Pablo Jiménez on 24/1/17.
+//  Copyright © 2017 Pablo Jiménez. All rights reserved.
+//
+
+import Foundation
